7d94f0db51cefb5fe91539254ff2fe55f3a1850b
SullivanC19 <colinsullivan203@gmail.com>
2024-05-19 00:07:25 -0700

    added auth

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { Image, View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Text, TextInput, Pressable, StyleSheet, Alert } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { RootStackParamList } from '../components/NavigationTypes';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import PhoneInput from "react-native-phone-number-input";
 
 type LoginPageNavigationProp = NavigationProp<RootStackParamList, 'LoginPage'>;
 
@@ -11,31 +13,93 @@ interface LoginPageProps {
 
 const LoginPage: React.FC<LoginPageProps> = ({ navigation }) => {
 
-  const handleLogin = async () => {
-    navigation.navigate('WorldPage')
+  const [rawPhoneNumber, setRawPhoneNumber] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
+  const phoneInput = useRef<PhoneInput>(null);
+
+  const [confirm, setConfirm] = useState<void | FirebaseAuthTypes.ConfirmationResult>();
+  const [code, setConfirmationCode] = useState<string>('');
+
+  // Check if user is already logged in
+  useEffect(() => {
+    auth().signOut();
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  const onAuthStateChanged = (user: any) => {
+    if (user) {
+      navigation.navigate('WorldPage');
+    }
   };
+
+  // Handle login and starts phone number verification process
+  async function handleLogin() {
+    if (!phoneNumber) return;
+    console.log(phoneNumber.toString());
+    const confirmation = await auth()
+    .signInWithPhoneNumber(phoneNumber.toString())
+    .catch((error: any) => Alert.alert('Invalid phone number.'));
+    setConfirm(confirmation);
+  };
+
+  // Handle confirmation of phone number
+  async function handleConfirm() {
+    if (confirm) {
+      await confirm
+      .confirm(code)
+      .catch((error: any) => {
+        Alert.alert('Invalid code.');
+        console.log(error);
+      });
+    }
+  }
+  
+  if (!confirm) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleText}>
+          DebDem
+        </Text>
+        <PhoneInput
+          ref={phoneInput}
+          defaultValue={rawPhoneNumber}
+          defaultCode="US"
+          layout="first"
+          onChangeText={(text) => {
+            setRawPhoneNumber(text);
+          }}
+          onChangeFormattedText={(text) => {
+            setPhoneNumber(text);
+          }}
+          withDarkTheme
+          withShadow
+          autoFocus
+          containerStyle={{width: '100%'}}
+        />
+        
+        <Pressable style={styles.loginButton} onPress={handleLogin}>
+          <View>
+              <Text style={styles.loginButtonText}>Enter</Text>
+          </View>
+        </Pressable>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>
         DebDem
       </Text>
-      <TextInput style={styles.input}
-        placeholder="Username"
-      />
-      <TextInput style={styles.input}
-        placeholder="Password"
+      <TextInput style={styles.input} onChangeText={setConfirmationCode}
+        placeholder="Confirmation Code"
       />
       
-      <Pressable style={styles.loginButton} onPress={handleLogin}>
+      <Pressable style={styles.loginButton} onPress={handleConfirm}>
         <View>
-            <Text style={styles.loginButtonText}>Log In</Text>
+            <Text style={styles.loginButtonText}>Enter</Text>
         </View>
-      </Pressable>
-      <Pressable>
-        <Text>
-          No Account? Sign up!
-        </Text>
       </Pressable>
     </View>
   );
