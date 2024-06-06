d08fee86f93e1a806bf1b9212de28b7a1a4a1812
SullivanC19 <colinsullivan203@gmail.com>
2024-06-05 20:39:30 -0700

    added outline for backend information pulling

--- src/database/Firestore.tsx
+++ src/database/Firestore.tsx
@@ -16,11 +16,35 @@ export function addVideoToRoom(roomId: string, cloudStorageVideoURI: string) {
   });
 }
 
-// TODO: for comments: comment, room id, video id, user id, timestamp
 
 export async function fetchVideosFromRoom(roomId: string) {
   // fetch videos from room
   console.log(`Fetching videos from room ${roomId}`);
   const videoRoomDoc = await firestore().collection("videos").doc(roomId).get();
   return (videoRoomDoc.get("videoPaths") as FirebaseFirestoreTypes.DocumentFieldType[]);
+}
+
+// video has uri, comments list, poster, etc,
+export async function fetchVideo(videoId: string) {
+  return null;
+}
+
+// world has list of questions (with vote counts and users who've voted), list of users in world, list of room ids, list of uninitialized room ids, list of questions
+export async function fetchWorld(worldId: string) {
+  return null;
+}
+
+// rooom has title question, description, list of video ids, list of users ids in room, time initialized
+export async function fetchRoom(roomId: string) {
+  return null;
+}
+
+// question has title question, description, yes/no votes, and list of user ids who've voted
+export async function fetchQuestion(questionId: string) {
+  return null;
+}
+
+// user has first/last name, phone number
+export async function fetchUser(userId: string) {
+  return null;
 }
